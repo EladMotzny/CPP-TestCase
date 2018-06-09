c6c3392d8fea6865a47564413e5bee4b02172bb4
alex <alexyafish@gmail.com>
2018-06-09 15:46:44 +0300

    Cerr problem

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,1 +1,10 @@
 #include "TestCase.hpp"
+using namespace std;
+
+// template <typename type, typename type> 
+// void check_equal(const type& t1, const type& t2) {
+// 	if(t1 != t2)
+//         this->cerr << this->type << ": Failure in test #1: " << type << " should equal " << type << "!" << endl;
+// } 
+
+
--- TestCase.hpp
+++ TestCase.hpp
@@ -1,2 +1,26 @@
 using namespace std;
+#include <iostream>
 #pragma once;
+#include <string>
+#include <sstream>
+
+// template <typename type>
+class TestCase{
+    public:
+        string msg;
+        ostream& cerr;
+
+         TestCase(const string msg, ostream& err): cerr(err){ //TestCase<type>::
+            this->msg = msg;
+            this->cerr <<"";
+        }
+   
+        template <typename type>
+        void check_equal(const type &t1, const type &t2) { //TestCase<type>::
+	    if(t1 != t2)
+            this->cerr << this->msg << ": Failure in test #1: " << type << " should equal " << type << "!" << endl;
+        }
+
+        void print(){this->cerr << endl;}
+
+};
